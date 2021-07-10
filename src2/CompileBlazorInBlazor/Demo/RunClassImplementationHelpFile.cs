1b22873430f620dd002ae01f642be7d413f92490
Praneet Mathur <63034836+pm-Architect@users.noreply.github.com>
2021-07-11 04:15:18 +0530

    Added implementation help file

--- src2/CompileBlazorInBlazor/Demo/RunClassImplementationHelpFile.cs
+++ src2/CompileBlazorInBlazor/Demo/RunClassImplementationHelpFile.cs
@@ -1,8 +1,25 @@
 ﻿
+using System.Drawing;
+
 namespace CompileBlazorInBlazor.Demo
 {
-    //public class MyRunClass2 : RunClass
+    //public class MyCommand : AbstractCommand
     //{
+    //    public override string Name => "Test Command 01";
+
+    //    public override string Author => "Hackathon";
+
+    //    public override string Description => "lol";
+
+    //    public override Color Accent => Color.OrangeRed;
+
+    //    public override string CommandLineName => "test";
+
+    //    public override void RegisterInputArguments()
+    //    {
+    //        //nothing yet
+    //    }
+
     //    public override void RunCommand(Context context)
     //    {
     //        context.AddCube(20, 20, 20, 20, 20, 20);
