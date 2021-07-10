34898dcc3ecb9949ef746d5c7d64943fc55bc971
Emil Poulsen <emil-poulsen@hotmail.com>
2021-07-10 17:52:40 +0200

    adding a three.js scene to the scene tab

---
+++ src2/CompileBlazorInBlazor/wwwroot/js/scene.js
@@ -0,0 +1,95 @@
+﻿var scene;
+var camera;
+var cube;
+var renderer;
+var material;
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var caller;
+var controls;
+
+function createScene(reference) {
+    window.addEventListener('click', onDocumentMouseDown, false);
+    caller = reference;
+    scene = new THREE.Scene();
+
+    scene.background = new THREE.Color(0xfafafa);
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    camera.position.set(0, 0, 50);
+
+    camera.up = new THREE.Vector3(0, 0, 1);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector("canvas"),
+        antialias: true,
+        preserveDrawingBuffer: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    var size = 50;
+    var divisions = 25;
+
+    var gridHelper = new THREE.GridHelper(size, divisions);
+    gridHelper.name = "GridHelper";
+    gridHelper.geometry.rotateX(Math.PI / 2);
+    scene.add(gridHelper);
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function onDocumentMouseDown(event) {
+    event.preventDefault();
+    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = - (event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(scene.children);
+    if (intersects.length > 0) {
+
+        intersects[0].object.callback();
+    }
+}
+
+function createCube() {
+    var geometry = new THREE.BoxGeometry();
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
+    cube.callback = function () { caller.invokeMethodAsync('OnClickCube', cube); };
+    scene.add(cube);
+    camera.position.z = 5;
+}
+
+function healthy() {
+    cube.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+}
+
+function unhealthy() {
+    cube.material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+}
+
+function clickCube() {
+    cube.material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+}
+
+
+var animate = function() {
+    requestAnimationFrame(animate);
+    controls.update();
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+};
