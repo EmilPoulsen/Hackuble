6b5ac78e79ef31fca17445d8bb1f0a0a835a3227
Emil Poulsen <emil-poulsen@hotmail.com>
2021-07-11 20:15:32 +0200

    moved commands into separate files

---
+++ src2/Hackuble.Examples/AddCubeDef.cs
@@ -0,0 +1,32 @@
+﻿namespace CompileBlazorInBlazor.Demo.Examples
+{
+    public class AddCubeDef : AbstractCommand
+    {
+        public override string Name => "Add Cube";
+
+        public override string Author => "Hackathon21";
+
+        public override string Description => "Add a cuboid to the scene";
+
+        public override string CommandLineName => "cubedef";
+
+        public override string Accent => "#ff6700";
+
+        public override void RegisterInputArguments(DataAccess dataAccess)
+        {
+            dataAccess.RegisterTextArgument("Color", "The color of the cube in Hex Format", "#ff6700");
+        }
+
+        public override CommandStatus RunCommand(Context context, DataAccess dataAccess)
+        {
+            string c = "#ffffff";
+            if (!dataAccess.GetData<string>(0, ref c))
+            {
+                return CommandStatus.Failure;
+            }
+
+            context.AddCube(20.0, 20.0, 20.0, 0, 0, 0, c);
+            return CommandStatus.Success;
+        }
+    }
+}
