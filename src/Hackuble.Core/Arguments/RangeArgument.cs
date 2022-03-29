349cdf335246eae01fa03774ae6eba4be5f8cde1
filipejsbrandao <filipejsbrandao@hotmail.com>
2022-03-29 20:12:28 +0100

    * RangeArgument.cs: A new range argument
    * AddCubeSliders.cs: A new example file with a range argument
    
    * DataAccess.cs: Changed to include the new range argument
    
    * NavMenu.razor: Added the range argument

---
+++ src/Hackuble.Core/Arguments/RangeArgument.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackuble.Arguments
+{
+    public class RangeArgument : AbstractArgument<double>
+    {
+        public RangeArgument(string prompt, string description, double defaultValue)
+            : this(prompt, description, defaultValue, defaultValue - 10, defaultValue +10)
+        {
+
+        }
+
+        public RangeArgument(string prompt, string description, double defaultValue, double minValue, double maxValue)
+            : base(prompt, description, defaultValue)
+        {
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+
+        public override void RenderArgumentInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
